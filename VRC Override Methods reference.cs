0adc8aad953781761a17a7fcb147d4d84c8c6177
Child of the Beast <dragon_5556@msn.com>
2020-06-11 13:05:17 -0400

    Create VRC Override Methods reference.cs

---
+++ VRC Override Methods reference.cs
@@ -0,0 +1,50 @@
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class OverrideList : UdonSharpBehaviour
+{
+    public override void Interact()
+    {
+        // runs when you Interact with an object
+    }
+
+    /*
+     * Pickups
+     * These must be attached to an object with VRC_Pickup on it
+     */
+    public override void OnPickup()
+    {
+        //Runs for the player that pickups the object
+    }
+    public override void OnDrop()
+    {
+        //Runs for the player that drops the object
+    }
+    public override void OnPickupUseDown()
+    {
+        //Runs for the player that uses the pickup
+    }
+    public override void OnPickupUseUp()
+    {
+        //Runs for the player that stops using the pickup
+    }
+
+    /*
+     * 
+     */
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        //Runs when a player joins
+        //Returns that player that joined
+    }
+
+    public override void OnPlayerLeft(VRCPlayerApi player)
+    {
+        //runs when a player leaves
+        //Returns that player that left
+    }
+
+}
